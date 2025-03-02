9a90403491d3ca2783403dbad2272027056397ec
Yamini214 <patidaryamini16@gmail.com>
2025-03-02 13:29:20 +0530

    Employee management project Complete

--- src/newDaoPattern/usecase/Demo2.java
+++ src/newDaoPattern/usecase/Demo2.java
@@ -1,7 +1,10 @@
 package newDaoPattern.usecase;
 
 import newDaoPattern.exception.EmployeeException;
+import newDaoPattern.model.Departments;
 import newDaoPattern.model.Employee;
+import newDaoPattern.service.DepartmentService;
+import newDaoPattern.service.DepartmentServiceImpl;
 import newDaoPattern.service.EmployeeService;
 import newDaoPattern.service.EmployeeServiceImpl;
 
@@ -11,36 +14,37 @@ public  class Demo2 {
 
     public static void main(String[] args) {
 
-        EmployeeService serivce=new EmployeeServiceImpl();
+        EmployeeService serivce = new EmployeeServiceImpl();
+
+
 //        //insert into table employee
 //
-//        Employee emp=new Employee(102,"Umesh  Pandey","rudrapandey@gmail.com","7322678",22);
+//        Employee emp=new Employee(103,"   Pandey","rudrapandey@gmail.com","7322678",22);
 //        String s =serivce.saveEmployee(emp);
-//
-
-//
 //        System.out.println(s);
+
+
 //select table data
-
-//        try {
-//            List<Employee> laptops =  serivce.getAllEmployees();
-//            System.out.println("All Employee:");
-//            for (Employee l : laptops) {
-//                System.out.println(l.getEmp_id() + " " + l.getEmp_name()
-//                        + " " + l.getEmp_email()+" "+l.getEmp_phone()+" "+l.getDept_id()+" ");
-//            }
-//        } catch (EmployeeException e) {
-//            System.out.println(e.getMessage());
-//        }
-
-
-//        UPDATE - Updating laptop details
-         Employee updatedEmployee=new Employee(101,"Rahul Patidar","patidarrahul@gmail.com"," .....2007",11);
-  try {
-            Employee updated = serivce.updateEmployee(updatedEmployee);
-            System.out.println("Updated Laptop: " +updated.getEmp_id()+" "+ updated.getEmp_name() + " " + updated.getEmp_email() + " " + updated.getEmp_phone() + " " + updated.getDept_id() + " " + " ");
+        try {
+            List<Employee> Employees = serivce.getAllEmployees();
+            System.out.println("All Employee:");
+            for (Employee l : Employees) {
+                System.out.println(l.getEmp_id() + " " + l.getEmp_name()
+                        + " " + l.getEmp_email() + " " + l.getEmp_phone() + " " + l.getDept_id() + " ");
+            }
         } catch (EmployeeException e) {
             System.out.println(e.getMessage());
         }
 
-    }}
+//        UPDATE - Updating employee details
+//        Employee updatedEmployee=new Employee(101,"Kirti Sharma","kirtisharma@gmail.com","9876785678",11);
+//  try {
+//            Employee updated = serivce.updateEmployee(updatedEmployee);
+//            System.out.println("Updated Employee: " +updated.getEmp_id()+" "+ updated.getEmp_name() + " " + updated.getEmp_email() + " " + updated.getEmp_phone() + " " + updated.getDept_id() + " " + " ");
+//        } catch (EmployeeException e) {
+//            System.out.println(e.getMessage());
+//        }
+    }
+}
+
+
