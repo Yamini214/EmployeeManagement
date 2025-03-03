085360d040f96f9098d9536f6eced20665f2acf6
Yamini214 <patidaryamini16@gmail.com>
2025-03-03 09:57:34 +0530

    Employee management project Complete

--- src/newDaoPattern/usecase/Demo2.java
+++ src/newDaoPattern/usecase/Demo2.java
@@ -15,8 +15,7 @@ public  class Demo2 {
     public static void main(String[] args) {
 
         EmployeeService service = new EmployeeServiceImpl();
-//
-//
+
 //       insert into table employee
 
          Employee emp=new Employee(102,"Trisha Patel","pateltrisha@gmail.com","7890983397",12);
@@ -38,46 +37,42 @@ public  class Demo2 {
 
 
 
-        //by name
-
 
         // Get employee by name
-//        String employeeName = "Kirti Sharma"; // Change this name as needed
-//        try {
-//            Employee emp = service.getEmployeeByName(employeeName);
-//            System.out.println("Employee Found: " + emp.getEmp_id() + "  " + emp.getEmp_name()
-//                    + "  " + emp.getEmp_email() + " " + emp.getEmp_phone() + " " + emp.getDept_id());
-//        } catch (EmployeeException e) {
-//            System.out.println(e.getMessage());
-//        }
+        String employeeName = "Trisha Patel";
+        try {
+            Employee empbyname = service.getEmployeeByName(employeeName);
+            System.out.println("Employee Found: " + empbyname
+                    .getEmp_id() + "  " + empbyname.getEmp_name()
+                    + "  " + empbyname.getEmp_email() + " " + empbyname.getEmp_phone() + " " + empbyname.getDept_id());
+        } catch (EmployeeException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
 //        UPDATE - Updating employee details
-//        Employee updatedEmployee=new Employee( 102," Trisha Patel","trishapatel@gmail.com","9873217890",13);
-//  try {
-//            Employee updated = serivce.updateEmployee(updatedEmployee);
-//            System.out.println("Updated Employee: " +updated.getEmp_id()+" "+ updated.getEmp_name() + " " + updated.getEmp_email() + " " + updated.getEmp_phone() + " " + updated.getDept_id() + " " + " ");
-//        } catch (EmployeeException e) {
-//            System.out.println(e.getMessage());
-//        }
+        Employee updatedEmployee=new Employee( 102," Trisha Patel","trishapatel@gmail.com","9873217890",13);
+  try {
+            Employee updated = service.updateEmployee(updatedEmployee);
+            System.out.println("Updated Employee: " +updated.getEmp_id()+" "+ updated.getEmp_name() + " " + updated.getEmp_email() + " " + updated.getEmp_phone() + " " + updated.getDept_id() + " " + " ");
+        } catch (EmployeeException e) {
+            System.out.println(e.getMessage());
+        }
 
 
 
-//
-//        Departments updatedepartments = new Departments(76, "IT");
-//  try {
-//
-//      Departments Update =  services.updatedepartments(updatedepartments);
-//            System.out.println("Updated Department: " +Update.getDepartmentId()+" "+ Update.getDepartmentName()  );
-//        } catch (EmployeeException e) {
-//            System.out.println(e.getMessage());
-//        }
+
         // DELETE EMPLOYEE
 //
-//        try{
-//            String deleteMessage = service.deleteEmployeeById(102);
-//            System.out.println(deleteMessage);
-//        }catch (EmployeeException e){
-//            System.out.println(e.getMessage());
-//        }
+        try{
+            String deleteMessage = service.deleteEmployeeById(102);
+            System.out.println(deleteMessage);
+        }catch (EmployeeException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
